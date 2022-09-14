5946431a5eee66008f64f91f111475ded0cfa2ee
Md. Abu Musa <musa.fci@gmail.com>
2022-09-14 23:53:42 +0600

    Add, Select, Complete Todo Functionalities
    Add shortid package.

--- src/components/todo/index.jsx
+++ src/components/todo/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import shortid from "shortid";
 import ListView from "../listView";
 import TableView from "../tableView";
+import CreateTodoForm from "../create-todo-form";
+import Controller from "../controllers";
+import {Modal, ModalBody, ModalHeader} from "reactstrap";
+
 
 class Todos extends React.Component {
 
@@ -22,16 +27,47 @@ class Todos extends React.Component {
                 isComplete: false,
                 isSelect: false
             },
-        ]
+        ],
+        isOpenTodoForm: false,
+        searchTerm: '',
     };
 
 
     toggleSelect = todoId => {
+        const todos = [...this.state.todos];
+        const todo = todos.find(t => t.id == todoId);
+        todo.isSelect = !todo.isSelect;
 
+        this.setState({ todos });
     }
 
     toggleComplete = todoId => {
+        const todos = [...this.state.todos];
+        const todo = todos.find(t => t.id == todoId);
+        todo.isComplete = !todo.isComplete;
 
+        this.setState({ todos });
+    }
+
+    toggleForm = () => {
+        this.setState({
+            isOpenTodoForm: !this.state.isOpenTodoForm
+        })
+    }
+
+    handleSearch = () => {
+
+    }
+
+    createTodo = todo => {
+        todo.id = shortid.generate();
+        todo.time = new Date();
+        todo.isComplete = false;
+        todo.isSelect = false;
+
+        const todos = [todo, ...this.state.todos];
+        this.setState({todos});
+        this.toggleForm();
     }
 
 
@@ -40,6 +76,12 @@ class Todos extends React.Component {
             <div>
                 <h1 className="display-4 text-center mb-5">React Todo</h1>
                 
+                <Controller
+                    term={this.state.searchTerm}
+                    toggleForm={this.toggleForm}
+                    handleSearch={this.handleSearch}
+                />
+
                 <div>
                     <ListView 
                         todos={this.state.todos} 
@@ -56,6 +98,19 @@ class Todos extends React.Component {
                     />
                 </div>
                 
+                <Modal
+                    isOpen={this.state.isOpenTodoForm}
+                    toggle={this.toggleForm}
+                >
+                    <ModalHeader toggle={this.toggleForm}>
+                        Create New Todo Item
+                    </ModalHeader>
+
+                    <ModalBody>
+                        <CreateTodoForm createTodo={this.createTodo} />
+                    </ModalBody>
+
+                </Modal>
             </div>
         );
     }
